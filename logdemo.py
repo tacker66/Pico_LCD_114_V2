0de7ec3b828c4b3d40006727ec4f1d3ba8cd6be4
Thomas <th.acker@arcor.de>
2023-09-04 13:41:24 +0000

    logdemo.py: efficient scrolling

--- logdemo.py
+++ logdemo.py
@@ -13,12 +13,6 @@ _height    = 10
 _start_x   = 0
 _start_y   = 0
 
-lines  = dict()
-errors = dict()
-for i in range(0, _num_lines):
-    lines[i]  = ""
-    errors[i] = 0
-
 def random_msg():
     num = random.randint(5, 25)
     msg = ""
@@ -27,35 +21,29 @@ def random_msg():
     return msg
 
 def _display(pos, msg, error):
-    h = _height
-    x = _start_x
-    y = _start_y + pos * (h + 1)
     fg = LCD.GREEN
     bg = LCD.BLACK
     if error > 0:
         fg = LCD.YELLOW
     if error > 1:
         fg = LCD.RED
-    lcd.rect(x, y, LCD.WIDTH, h+1, bg, True)
+    h = _height + 1
+    x = _start_x
+    y = _start_y + pos * h
+    lcd.rect(x, y, LCD.WIDTH, h, bg, True)
     lcd.text(msg, x, y, fg)
 
 _pos = 0
 def display(msg, error):
     global _pos
-    lines[_pos]  = msg
-    errors[_pos] = error
     _display(_pos, msg, error)
     _pos = _pos + 1
     if _pos >= _num_lines:
+        lcd.scroll(0, -_height)
         _pos = _num_lines-1
-        for i in range(0, _pos):
-            lines[i]  = lines[i+1]
-            errors[i] = errors[i+1]
-            _display(i, lines[i], errors[i])
         _display(_pos, "", 0)
     lcd.show()
         
 while True:
     display(random_msg(), random.randint(0, 3))
     time.sleep_ms(300)
-    
